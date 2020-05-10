f8b78edd3bc5d78019dc85c21bf1516190f5c508
Scott  Keplinger <skeplin17@gmail.com>
2020-05-10 19:14:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,4 +1,4 @@
-require pry
+require 'pry'
 
 def new_hash
  brand_new_hash = {}
@@ -24,6 +24,6 @@ end
 
 def monopoly_with_fourth_tier
 	monopoly = {railroads: {pieces: 4, names:{reading_railroad: {"mortgage_value"=>"$100"}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline: {}}, rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}}}
-
+binding.pry
 
 end
